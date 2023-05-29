c3d664bd6a7fd0f1b42fe51029eb829db441fab5
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2023-05-29 23:10:21 +0300

    fixed length stack

---
+++ Exam Revision/Fixed Length Stack/FixedLengthStack.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+template <typename T, const size_t capacity>
+class FixedLengthStack {
+
+private:
+
+	T data[capacity];
+	size_t size = 0;
+
+public:
+	
+	void push(const T& obj);
+	void push(T&& obj);
+
+	void pop();
+	const T& peek() const;
+	bool isEmpty() const;
+	size_t getSize() const;
+	void clear();
+
+};
+
+template <typename T, const size_t capacity>
+void FixedLengthStack<T, capacity>::push(const T& obj) {
+
+	if (size == capacity) {
+		throw std::exception("Stack is already full!");
+	}
+
+	data[size++] = obj;
+}
+
+template <typename T, const size_t capacity>
+void FixedLengthStack<T, capacity>::push(T&& obj) {
+
+	if (size == capacity) {
+		throw std::exception("Stack is already full!");
+	}
+
+	data[size++] = std::move(obj);
+}
+
+template <typename T, const size_t capacity>
+void FixedLengthStack<T, capacity>::pop() {
+
+	if (isEmpty()) {
+		throw std::exception("Stack is empty!");
+	}
+
+	size--;
+}
+
+template <typename T, const size_t capacity>
+const T& FixedLengthStack<T, capacity>::peek() const {
+
+	if (isEmpty()) {
+		throw std::exception("Stack is already full!");
+	}
+
+	return data[size - 1];
+}
+
+template <typename T, const size_t capacity>
+bool FixedLengthStack<T, capacity>::isEmpty() const {
+
+	return size == 0;
+}
+
+template <typename T, const size_t capacity>
+size_t FixedLengthStack<T, capacity>::getSize() const {
+
+	return size;
+}
+
+template <typename T, const size_t capacity>
+void FixedLengthStack<T, capacity>::clear() {
+	size = 0;
+}
