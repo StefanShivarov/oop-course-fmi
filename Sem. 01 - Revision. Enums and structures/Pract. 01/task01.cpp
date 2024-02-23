1680186ae49e2faa73493368fdbc38b501850068
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-02-23 19:00:28 +0200

    add complex number task

---
+++ Sem. 01 - Revision. Enums and structures/Pract. 01/task01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct ComplexNumber {
+	int real = 0;
+	int imaginary = 0;
+};
+
+ComplexNumber sumComplexNumbers(const ComplexNumber& lhs, const ComplexNumber& rhs) {
+	return { lhs.real + rhs.real, lhs.imaginary + rhs.imaginary };
+}
+
+ComplexNumber multiplyComplexNumbers(const ComplexNumber& lhs, const ComplexNumber& rhs) {
+	return {
+		lhs.real * rhs.real - lhs.imaginary * rhs.imaginary,
+		lhs.real * rhs.imaginary + lhs.imaginary * rhs.real
+	};
+}
+
+void printComplex(const ComplexNumber& complex) {
+	std::cout << complex.real
+		<< (complex.imaginary < 0 ? "" : " +")
+		<< " " << complex.imaginary << "i";
+}
+
+int main() {
+	ComplexNumber c1{ 0, 4 };
+	ComplexNumber c2{ -6, 1 };
+
+	printComplex(sumComplexNumbers(c1, c2));
+	std::cout << std::endl;
+	printComplex(multiplyComplexNumbers(c1, c2));
+
+	return 0;
+}
