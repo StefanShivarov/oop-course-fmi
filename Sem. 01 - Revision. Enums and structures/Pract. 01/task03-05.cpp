08bda3e7b2731e2d8a788a18a49ff9fcddfe0eea
Stefan Shivarov <stefan.shivarov.jr@gmail.com>
2024-02-23 21:18:26 +0200

    add tasks 03 - 05

--- Sem. 01 - Revision. Enums and structures/Pract. 01/task03-05.cpp
+++ Sem. 01 - Revision. Enums and structures/Pract. 01/task03-05.cpp
@@ -10,12 +10,12 @@ enum class Year {
 };
 
 struct Student {
-    unsigned fn;
-    Year year;
-    double averageGrade;
+    unsigned fn = 0;
+    Year year = Year::Unknown;
+    double averageGrade = 0.0;
 };
 
-Year getYearFromNumber(int number) {
+Year getYearFromNumber(unsigned number) {
     switch (number) {
         case 1: return Year::First;
         case 2: return Year::Second;
@@ -39,9 +39,16 @@ bool isValidFn(unsigned fn) {
     return fn > 9999 && fn < 100000;
 }
 
-Student initStudent(unsigned fn, int year, double averageGrade) {
+Student initStudent(unsigned fn, unsigned year, double averageGrade) {
     assert(isValidFn(fn));
     return { fn, getYearFromNumber(year), averageGrade };
+}
+
+void readStudent(Student& student) {
+    unsigned fn, year;
+    double avg;
+    std::cin >> fn >> year >> avg;
+    student = initStudent(fn, year, avg);
 }
 
 void printStudent(const Student& student) {
@@ -51,11 +58,65 @@ void printStudent(const Student& student) {
 	    << std::endl;
 }
 
+const int MAX_STUDENTS_CAPACITY = 10;
+
 struct Group {
-    
+    unsigned studentsAmount = 0;
+    Student students[MAX_STUDENTS_CAPACITY];
+    double averageGrade = 0.0;
 };
 
-int main() {
-    Student student = initStudent(12345, 2, 4.50);
-    printStudent(student); //Fn: 12345, Year: Second, Average grade: 4.50
+void readGroup(Group& group) {
+    std::cin >> group.studentsAmount;
+    double sumOfAvgGrades = 0.0;
+
+    for (unsigned i = 0; i < group.studentsAmount; i++) {
+        readStudent(group.students[i]);
+        sumOfAvgGrades += group.students[i].averageGrade;
+    }
+
+    group.averageGrade = sumOfAvgGrades / group.studentsAmount;
 }
+
+unsigned getAmountOfStudentsWithScholarship(const Group& group, double requiredGrade) {
+    unsigned result = 0;
+    for (unsigned i = 0; i < group.studentsAmount; i++) {
+        if (group.students[i].averageGrade >= requiredGrade) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+void sortStudentsInGroup(Group& group, bool(*comparatorFn)(const Student& lhs, const Student& rhs)) {
+    for (unsigned i = 0; i < group.studentsAmount; i++) {
+        unsigned minIndex = i;
+        for (unsigned j = i + 1; j < group.studentsAmount; j++) {
+            if (comparatorFn(group.students[j], group.students[minIndex])) {
+                minIndex = j;
+            }
+        }
+
+        if (i != minIndex) {
+            std::swap(group.students[i], group.students[minIndex]);
+        }
+    }
+}
+
+void printStudentsWithScholarshipSortedByGrade(Group& group, double requiredGrade) {
+    sortStudentsInGroup(group, [](const Student& lhs, const Student& rhs) { return lhs.averageGrade > rhs.averageGrade; });
+    for (unsigned i = 0; i < group.studentsAmount; i++) {
+        if (group.students[i].averageGrade >= requiredGrade) {
+            printStudent(group.students[i]);
+        }
+    }
+}
+
+/*
+int main() {
+    Group g;
+    readGroup(g);
+    printStudentsWithScholarshipSortedByGrade(g, 5.50);
+}
+*/
--- Sem. 01 - Revision. Enums and structures/Pract. 01/task04.cpp
+++
@@ -1,2 +0,0 @@
-#include "task03.cpp"
-
